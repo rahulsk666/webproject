ab7e098777e102b68207596c85ad60952d58380b
Rahul S Kumar <84220247+rahulsk666@users.noreply.github.com>
2021-05-17 20:26:45 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function purple_btn()
+{
+    document.body.style.background="red"
+
+}
+function green_btn()
+{
+    document.body.style.background="green"    
+}
+function yellow_btn()
+{
+    document.body.style.background="yellow"
+}
